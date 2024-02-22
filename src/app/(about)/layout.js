f87170010d683a0f746ec8f4a1e21e51594c85e1
cuongvongocfe <130651676+cuongvongocfe@users.noreply.github.com>
2024-02-22 11:22:09 +0700

    Update layout.js

--- src/app/(about)/layout.js
+++ src/app/(about)/layout.js
@@ -2,13 +2,9 @@ import InsightRoll from "@/src/components/About/InsightRoll";
 
 
 const insights = [
-    "20+ Projects Completed",
-    "3+ Years of Freelancing",
-    "99% Client Satisfaction",
-    "20K+ Subscribers",
-    "Authored In-Depth Course on Educative",
-    "Contributed as a Technical Course Reviewer 📝",
-    "Recipient of the Hackernoon Noonies Award 🏆",
+    "20+ Hợp đồng",
+    "1+ Years of Freelancin",
+    "99% Sự Hài Lòng Của Khách Hàng"
   ];
 
 export default function AboutLayout({ children }) {
