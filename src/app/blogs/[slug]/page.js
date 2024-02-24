6598549489e116317880cea8e46003f5b92b4309
cuongvongocfe <130651676+cuongvongocfe@users.noreply.github.com>
2024-02-24 16:06:02 +0700

    Update page.js

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -130,7 +130,7 @@ export default function BlogPage({ params }) {
             open
           >
             <summary className="text-lg font-semibold capitalize cursor-pointer">
-              Table Of Content
+              Mục Lục
             </summary>
             <ul className="mt-4 font-in text-base">
               {blog.toc.map((heading) => {
